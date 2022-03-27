abf64fa3f2dd07e0961518c6c2bfd01fb19c5148
suraj0901 <suraj12345jha67890@gmail.com>
2022-03-27 15:53:39 +0530

    4 files modified

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 import express from 'express';
+import http from 'http';
 import { resolve } from 'path';
 import { add, get } from './util/database.js';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = 3000;
 
 app.use(express.static('static'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const sendAllMessages = async () => {
+  const [success, error] = await get();
+  if (error) console.log(error);
+  else {
+    io.emit('allPrevMessage', success);
+  }
+};
+
+const addMessage = async (msg) => {
+  const [success, error] = await add(req.body);
+  if (error) console.log(error);
+  else {
+    io.emit('newMessage', msg);
+  }
+};
+
+io.on('connection', async (socket) => {
+  await sendAllMessages();
+  socket.on('newMessage', addMessage);
+});
+
 app.get('/', (req, res) => {
-  console.log('hello world');
   res.sendFile(resolve('pages/index.html'));
 });
 
-app.get('/message', async (req, res) => {
-  const [success, error] = await get();
-  if (error) res.status(500);
-  else res.send(success);
-});
-
-app.post('/message', async (req, res) => {
-  console.log(req.body);
-  const [success, error] = await add(req.body);
-  if (error) res.status(500);
-  else res.status(200);
-});
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- util/database.js
+++ util/database.js
@@ -4,16 +4,13 @@ import { resolve } from 'path';
 const dbfile = resolve('static/data.json');
 
 export const add = async (newData) => {
-  console.log('ADD');
   let error = false,
     success = false;
   try {
     const file = await readFileSync(dbfile, 'utf8');
     const db = await JSON.parse(file);
     db.push(newData);
-    console.log(db);
     await writeFileSync(dbfile, JSON.stringify(db));
-    console.log('success');
     success = true;
   } catch (err) {
     error = err;
@@ -22,11 +19,11 @@ export const add = async (newData) => {
 };
 
 export const get = async () => {
-  console.log('GET');
   let error = false,
     success = false;
   try {
     success = await readFileSync(dbfile, 'utf8');
+    success = JSON.parse(success);
   } catch (err) {
     error = err;
   } finally {
