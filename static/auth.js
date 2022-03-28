665b8ceff713cdebf85f86687e352c0a6199cad6
suraj0901 <suraj12345jha67890@gmail.com>
2022-03-28 19:31:26 +0530

    1 file modified, 2 deleted

--- static/auth.js
+++ static/auth.js
@@ -25,7 +25,7 @@ auth.addEventListener('submit', async (e) => {
   if (_name.value && password.value) {
     const result = await varifyUser(_name.value, password.value);
     if (result) {
-      await import('./socket.js');
+      await init(result);
       auth.classList.toggle('hide');
     } else {
       authMsg.textContent = `Wrong Crendentials`;
@@ -33,3 +33,48 @@ auth.addEventListener('submit', async (e) => {
     }
   }
 });
+
+async function inti(token) {
+  const { io } = import('https://cdn.socket.io/4.4.1/socket.io.esm.min.js');
+
+  const socket = io({
+    auth: {
+      token,
+    },
+  });
+
+  const form = document.getElementById('form');
+  const message = document.getElementById('input');
+  const container = document.getElementById('container');
+
+  const addMessage = ({ name, message }) => {
+    const div = document.createElement('div');
+    div.className = 'msgbox';
+    div.innerHTML = `
+    <p class="name">${name}</p>
+    <p class="msg">${message}</p>
+  `;
+    container.appendChild(item);
+  };
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (message.value) {
+      socket.emit('message', {
+        token: localStorage.getItem('token'),
+        message: message.value,
+      });
+      message.value = '';
+    }
+  });
+
+  socket.on('allPrevMessage', function (msgs) {
+    for (const message of msgs) addMessage(message);
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+
+  socket.on('newMessage', function (msg) {
+    addMessage(msg);
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+}
